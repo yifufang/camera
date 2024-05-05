99ce72351d3e3e7a50eefbe0214bc09498da902a
yifufang <yffang01@gmail.com>
2024-05-04 20:36:32 -0700

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
-FROM python:3.11.5-slim
+FROM python:3.11.5
 
 # Define environment variable
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
-
 RUN pip install --upgrade pip
 
 WORKDIR /app
@@ -12,6 +11,8 @@ RUN apt-get update && apt-get install -y \
     gcc \
     default-libmysqlclient-dev \
     pkg-config \
+    libgl1-mesa-glx \
+    ffmpeg \
     && rm -rf /var/lib/apt/lists/*
 
 COPY ./requirements.txt .
--- camera/settings.py
+++ camera/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@n4o0a+g!zv#_g%gyciimv7h!@fuk!%s-swa9^5nr&uon=63(9'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
